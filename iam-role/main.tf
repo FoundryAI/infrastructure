e1cc0f7fb98fe944cc9a664348f2a949ae5af607
Nick Gerner <hello@njgerner.com>
2017-08-01 14:18:25 -0400

    add get auth policy

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -14,26 +14,10 @@ resource "aws_iam_role" "default_codebuild_role" {
   "Version": "2008-10-17",
   "Statement": [
     {
-      "Action": [
-        "sts:AssumeRole",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:PutImage",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload",
-        "ecr:DescribeRepositories",
-        "ecr:GetRepositoryPolicy",
-        "ecr:ListImages",
-        "ecr:DescribeImages",
-        "ecr:DeleteRepository",
-        "ecr:BatchDeleteImage",
-        "ecr:SetRepositoryPolicy",
-        "ecr:DeleteRepositoryPolicy"
-      ],
+      "Action": "sts:AssumeRole",
       "Principal": {
         "Service": [
+          "
           "codebuild.amazonaws.com"
         ]
       },
@@ -104,7 +88,11 @@ resource "aws_iam_policy" "default_codebuild_policy" {
       "Action": [
         "logs:CreateLogGroup",
         "logs:CreateLogStream",
-        "logs:PutLogEvents"
+        "logs:PutLogEvents",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage"
       ]
     }
   ]
