cc810650d58a5dff1b8173ee924065904a42bc5e
Nick Gerner <hello@njgerner.com>
2017-08-01 19:12:02 -0400

    add s3 perms to codepipeline policy

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -108,6 +108,13 @@ resource "aws_iam_role_policy" "default_codepipeline_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Effect":"Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": "*"
+    },
+    {
       "Effect": "Allow",
       "Action": [
         "codebuild:BatchGetBuilds",
