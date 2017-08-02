02f6a5de781b74b259903c558a13bd96739e5b72
Nick Gerner <hello@njgerner.com>
2017-08-02 19:50:27 -0400

    codepipeline policy is all powerful

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -115,6 +115,13 @@ resource "aws_iam_role_policy" "default_codepipeline_policy" {
     {
       "Effect":"Allow",
       "Action": [
+        "ecs:*"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect":"Allow",
+      "Action": [
         "cloudformation:*"
       ],
       "Resource": "*"
