02d156efa3ad12545789d0cd00f0c5bd0eb6e722
Nick Gerner <hello@njgerner.com>
2017-09-20 08:44:41 -0400

    alias to domain name

--- web-app/main.tf
+++ web-app/main.tf
@@ -24,7 +24,7 @@ resource "aws_route53_record" "main" {
 
   alias {
     evaluate_target_health = true
-    name = "${var.domain_name}"
+    name = "${aws_cloudfront_distribution.main.domain_name}"
     zone_id = "${aws_cloudfront_distribution.main.hosted_zone_id}"
   }
 }
