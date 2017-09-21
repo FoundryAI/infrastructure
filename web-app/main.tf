3e6ea451cdd2da0981dc78575d356e8c40351db9
Nick Gerner <hello@njgerner.com>
2017-09-21 11:34:12 -0400

    update base image for codebuild web app

--- web-app/main.tf
+++ web-app/main.tf
@@ -177,7 +177,7 @@ resource "aws_codebuild_project" "main" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0"
+    image        = "node:8.2"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
