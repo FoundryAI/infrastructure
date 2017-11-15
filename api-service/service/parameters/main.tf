0d3b4b8c73fa171818383c7314e759c2fc513597
Andrew Schmadel <andrew@foundry.ai>
2017-11-15 15:05:45 -0500

    standalone xray EC2 instance

---
+++ api-service/service/parameters/main.tf
@@ -0,0 +1,27 @@
+variable service {}
+
+variable environment {}
+
+variable parameters {
+  type = "map"
+}
+
+variable secrets {
+  type = "map"
+}
+
+resource "aws_ssm_parameter" "string" {
+  count = "${length(keys(var.parameters))}"
+
+  name = "/${var.environment}/${var.service}/env/${element(keys(var.parameters), count.index)}"
+  value = "${lookup(var.parameters, element(keys(var.parameters), count.index))}"
+  type  = "String"
+}
+
+resource "aws_ssm_parameter" "secret" {
+  count = "${length(keys(var.secrets))}"
+
+  name = "/${var.environment}/${var.service}/env/${element(keys(var.secrets), count.index)}"
+  value = "${lookup(var.secrets, element(keys(var.secrets), count.index))}"
+  type  = "SecureString"
+}
---
+++ xray/main.tf
@@ -0,0 +1,102 @@
+resource "aws_instance" "xray" {
+  ami = "ami-6057e21a"
+  instance_type = "t2.small"
+
+  key_name = "${var.environment}"
+  subnet_id = "${var.subnet_id}"
+  vpc_security_group_ids = [
+    "${aws_security_group.xray.id}"
+  ]
+  monitoring = true
+  iam_instance_profile = "${aws_iam_instance_profile.xray.id}"
+  user_data = <<USERDATA
+#!/bin/bash
+curl https://s3.dualstack.us-east-2.amazonaws.com/aws-xray-assets.us-east-2/xray-daemon/aws-xray-daemon-2.x.rpm -o /home/ec2-user/xray.rpm
+yum install -y /home/ec2-user/xray.rpm
+sed -i -e 's/UDPAddress.*$/UDPAddress: "0.0.0.0:2000"/' /etc/amazon/xray/cfg.yaml
+pkill xray
+USERDATA
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = "10"
+  }
+
+  tags {
+    Name = "xray-${var.environment}"
+    Environment = "${var.environment}"
+  }
+
+}
+
+resource "aws_security_group" "xray" {
+  name = "${format("%s-internal-xray", var.environment)}"
+  vpc_id = "${var.vpc_id}"
+  description = "Allows internal xray & ssh traffic"
+
+  ingress {
+    from_port = 2000
+    to_port = 2000
+    protocol = "udp"
+    cidr_blocks = [
+      "${var.cidr}"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${format("%s internal xray", var.environment)}"
+    Environment = "${var.environment}"
+  }
+}
+
+data "aws_iam_policy_document" "xray_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = [
+        "ec2.amazonaws.com",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "xray" {
+  name = "${var.environment}-xray-role"
+  assume_role_policy = "${data.aws_iam_policy_document.xray_assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
+  role       = "${aws_iam_role.xray.id}"
+}
+
+resource "aws_iam_instance_profile" "xray" {
+  name = "xray-instance-profile-${var.environment}"
+  path = "/"
+  role = "${aws_iam_role.xray.name}"
+}
+
+resource "aws_route53_record" "xray" {
+  name = "xray"
+  type = "A"
+  zone_id = "${var.dns_zone_id}"
+  records = ["${aws_instance.xray.private_ip}"]
+  ttl = 60
+}
