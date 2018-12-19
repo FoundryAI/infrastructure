4a0cad899551108608586885561998a9f170583d
Chris Wynnyk <wynnyk@foundry.ai>
2018-12-19 14:52:16 -0500

    Address PR feedback

--- s3/main.tf
+++ s3/main.tf
@@ -4,22 +4,28 @@ variable "name" {
 variable "environment" {
 }
 
-variable "account_id" {
+variable "log_bucket" {
+  description = "S3 bucket ID to write S3 access logs into"
 }
 
-resource "aws_kms_key" "transfer_s3_encrypt_key" {
+resource "aws_kms_key" "s3_encrypt_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
 }
 
-resource "aws_s3_bucket" "transfer" {
-  bucket = "${var.name}-${var.environment}-transfer"
+resource "aws_s3_bucket" "s3" {
+  bucket = "${var.name}-${var.environment}"
   acl    = "private"
+
+  logging {
+    target_bucket = "${var.log_bucket}"
+    target_prefix = "log/${var.name}/"
+  }
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.transfer_s3_encrypt_key.arn}"
+        kms_master_key_id = "${aws_kms_key.s3_encrypt_key.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
