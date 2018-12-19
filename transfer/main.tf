5a8398b6b027fe4ced51557687a2e76c9ac8ffb0
Chris Wynnyk <wynnyk@foundry.ai>
2018-12-19 13:52:30 -0500

    Added encryption, versioning

--- transfer/main.tf
+++ transfer/main.tf
@@ -16,12 +16,29 @@ data "template_file" "policy" {
   }
 }
 
+resource "aws_kms_key" "transfer_s3_encrypt_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "transfer" {
   bucket = "${var.name}-${var.environment}-transfer"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.transfer_s3_encrypt_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
   tags {
     Name = "${var.name}"
     Environment = "${var.environment}"
+  }
+
+  versioning {
+    enabled = true
   }
   
   policy = "${data.template_file.policy.rendered}"
