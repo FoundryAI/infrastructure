3295754029eaf890ef9c0fa647f99ae995080e02
Chris Wynnyk <wynnyk@foundry.ai>
2018-12-28 14:54:53 -0500

    Change from AWS Linux -> Ubuntu

--- bastion/user_data.sh
+++ bastion/user_data.sh
@@ -7,20 +7,13 @@ Host *
   User ubuntu
 EOF
 
-# Threatstack: 1. Add repo location
-THREATSTACK_YUM_REPO="/etc/yum.repos.d/threatstack.repo"
-cat <<EOF > $THREATSTACK_YUM_REPO
-[threatstack]
-name=Threat Stack Package Repository
-baseurl=https://pkg.threatstack.com/Amazon
-enabled=1
-gpgcheck=1
-EOF
+# Threatstack (Ubuntu agent install)
+# 1. Add the PGP key
+curl https://app.threatstack.com/APT-GPG-KEY-THREATSTACK | sudo apt-key add -
 
-# Threatstack: 2. Import PGP Key
-sudo wget https://app.threatstack.com/RPM-GPG-KEY-THREATSTACK -O /etc/pki/rpm-gpg/RPM-GPG-KEY-THREATSTACK
-sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-THREATSTACK
+# 2. Add repository info
+echo "deb https://pkg.threatstack.com/Ubuntu `lsb_release -c | cut -f2` main" | sudo tee /etc/apt/sources.list.d/threatstack.list > /dev/null
 
-# Threatstack: 3. Install and configure the agent
-sudo yum install threatstack-agent
+# 3. Install and configure the agent
+sudo apt-get update && sudo apt-get install threatstack-agent -y && \
 sudo cloudsight setup --deploy-key=29c5e5a38c36fbc45cd3325ca737b678hsR6utKVzwUeBFTeTO284T9vrPITbMX9LpldMBwt --ruleset="Base Rule Set" --agent_type=i
