9d4a93478d9f7de065d2f0041d253b2349bb270b
Chris Wynnyk <wynnyk@foundry.ai>
2019-01-02 11:20:13 -0500

    Auto-approve install for Threatstack on ECS

--- ecs-cluster/files/threatstack.sh
+++ ecs-cluster/files/threatstack.sh
@@ -5,7 +5,7 @@ set -e
 # Threatstack (AWS Linux agent install)
 # 1. Add repo location
 THREATSTACK_YUM_REPO="/etc/yum.repos.d/threatstack.repo"
-cat <<EOF > $THREATSTACK_YUM_REPO
+dcat <<EOF > $THREATSTACK_YUM_REPO
 [threatstack]
 name=Threat Stack Package Repository
 baseurl=https://pkg.threatstack.com/Amazon
@@ -18,5 +18,6 @@ sudo wget https://app.threatstack.com/RPM-GPG-KEY-THREATSTACK -O /etc/pki/rpm-gp
 sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-THREATSTACK
  
 # 3. Install and configure the agent
-sudo yum install threatstack-agent &&\
+sudo yum -y install threatstack-agent &&\
 sudo cloudsight setup --deploy-key=29c5e5a38c36fbc45cd3325ca737b678hsR6utKVzwUeBFTeTO284T9vrPITbMX9LpldMBwt --ruleset="Base Rule Set" --agent_type=i
+  
