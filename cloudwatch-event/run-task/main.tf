5b053d21bc19c6a94d87e10d52edeec9738e4553
Peter <peter@foundry.ai>
2019-01-07 14:19:04 -0500

    vars from worker service, rough cloudwatch template

---
+++ cloudwatch-event/run-task/main.tf
@@ -0,0 +1,29 @@
+resource "aws_cloudwatch_event_target" "yada" {
+  target_id = "Yada"
+  rule      = "${aws_cloudwatch_event_rule.console.name}"
+  arn       = "${aws_kinesis_stream.test_stream.arn}"
+
+  run_command_targets {
+    key    = "tag:Name"
+    values = ["FooBar"]
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "console" {
+  name        = "capture-ec2-scaling-events"
+  description = "Capture all EC2 scaling events"
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.autoscaling"
+  ],
+  "detail-type": [
+    "EC2 Instance Launch Successful",
+    "EC2 Instance Terminate Successful",
+    "EC2 Instance Launch Unsuccessful",
+    "EC2 Instance Terminate Unsuccessful"
+  ]
+}
+PATTERN
+}
