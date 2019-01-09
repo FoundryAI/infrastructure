7bc6567621415123f004bb0b6a402cf084cc4383
Peter <peter@foundry.ai>
2019-01-09 13:54:05 -0500

    update iam setup

--- cloudwatch-event/run-ecs-task/main.tf
+++ cloudwatch-event/run-ecs-task/main.tf
@@ -2,6 +2,10 @@ data "aws_iam_policy_document" "ecs_policy" {
   statement {
     effect  = "Allow"
     actions = ["ECS:*"]
+    principals {
+      type = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
   }
 
   statement {
