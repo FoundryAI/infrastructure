4065f9f7ac85598757eb233d9b4526981e2be292
Peter <peter@foundry.ai>
2019-01-09 13:59:49 -0500

    assume role only

--- cloudwatch-event/run-ecs-task/main.tf
+++ cloudwatch-event/run-ecs-task/main.tf
@@ -1,14 +1,5 @@
 data "aws_iam_policy_document" "ecs_policy" {
   statement {
-    effect  = "Allow"
-    actions = ["ECS:*"]
-    principals {
-      type = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-  }
-
-  statement {
     effect = "Allow"
     actions = ["sts:AssumeRole"]
     principals {
